6dc4a75aef5ac5cb7df3dc2db5cbc808dc9e0349
Umran Haji <unhaji@gmail.com>
2016-06-26 17:25:06 -0400

    added fil_parameters.py

---
+++ fil_parameters.py
@@ -0,0 +1,43 @@
+import subprocess
+import argparse
+
+#This script takes in a .fil file and uses the SIGPROC header function to return various parameters of it.
+parser = argparse.ArgumentParser(description='Extract parameters from .fil files')
+parser.add_argument('filename', type=str, help='Path to .fil file to be read')
+args = parser.parse_args()
+
+filename=args.filename
+
+def band_classifier(x): #Uses 'x', the middle frequency of the spectrum, to determine the band
+    #x must be in MHz!
+    if 1000 <= x < 2000:
+        band = 'L'
+    elif 2000 <= x < 4000:
+        band = 'S'
+    elif 4000 <= x < 8000:
+        band = 'C'
+    elif 8000 <= x < 12000:
+        band = 'X'
+    elif 12000 <= x < 18000:
+        band = 'Ku'
+    elif 18000 <= x < 27000:
+        band = 'K'
+    return band
+
+def fil_parameters(filename):
+    fmax=float(subprocess.check_output(['/usr/local/sigproc/bin/header', filename, '-fch1'])) #Obtains freq (MHz) of first channel from SIGPROC header function
+    nchans=float(subprocess.check_output(['/usr/local/sigproc/bin/header', filename, '-nchans'])) #Number of channels, from SIGPROC header
+    ch_bandwidth=float(subprocess.check_output(['/usr/local/sigproc/bin/header', filename, '-foff'])) #Channel bandwitch, from SIGPROC header
+
+    bandwidth = (nchans)*(ch_bandwidth) #Bandwidth of the entire spectrum from the .fil file
+    fmin = fmax + bandwidth
+
+    fmid = (fmin + fmax)/2.0
+    band = band_classifier(fmid)
+
+    fil_parameters = {'fmin':fmin, 'fmax':fmax, 'bandwidth':abs(bandwidth), 'band':band} 
+   
+    return fil_parameters
+
+print fil_parameters(filename)
+
